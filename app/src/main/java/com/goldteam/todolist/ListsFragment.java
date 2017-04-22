819e82393bb0d0b2b8b139900860246a949edf54
Drew Cobb <Drew Cobb>
2017-04-22 12:15:47 -0400

    Populated list of todo lists from database

--- app/src/main/java/com/goldteam/todolist/ListsFragment.java
+++ app/src/main/java/com/goldteam/todolist/ListsFragment.java
@@ -7,7 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.goldteam.todolist.Database.DatabaseHelper;
+
+import java.util.ArrayList;
 
 
 public class ListsFragment extends ListFragment {
@@ -16,19 +21,28 @@ public class ListsFragment extends ListFragment {
     }
 
     private ListsListener listener;
+    private DatabaseHelper db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String[] names = new String[List.LISTs.length];
-        for (int i = 0; i < names.length; i++) {
-            names[i] = List.LISTs[i].getName();
-        }
-
+        db = new DatabaseHelper(inflater.getContext());
+        java.util.List<String> lists = new ArrayList<>();
+        lists = db.readLists();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                names);
+                android.R.layout.simple_expandable_list_item_1,
+                lists);
+
+//        String[] names = new String[List.LISTs.length];
+//        for (int i = 0; i < names.length; i++) {
+//            names[i] = List.LISTs[i].getName();
+//        }
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+//                inflater.getContext(),
+//                android.R.layout.simple_list_item_1,
+//                names);
         setListAdapter(adapter);
 
         return super.onCreateView(inflater, container, savedInstanceState);
